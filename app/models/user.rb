aa200cf5179e01f867873d438422884f9db2f9b1
7yugatateno1 <7yuga1@gmail.com>
2023-02-03 01:24:32 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
+  validates :last_name, :first_name, format: { with: FULL_WIDTH_REGEX, message: "は全角で入力して下さい" }
+  validates :last_name_kana, :first_name_kana, format: { with: FULL_WIDTH_KANA_REGEX, message: "はカタカナ（全角）で入力して下さい" }
 end
