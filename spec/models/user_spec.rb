1d2ab75e8ce6d4ff16ad6d172788c82c23b0f0f0
7yugatateno1 <7yuga1@gmail.com>
2023-02-07 01:38:39 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe User, type: :model do
 
   describe "ユーザー新規登録" do
 
+    context '新規登録できる場合' do
+
+      it "全ての項目の入力が存在すれば登録できる" do
+        @user = FactoryBot.build(:user)
+      end
+    
+    end 
+
     context '新規登録できない場合' do
 
       it "nicknameが空だと登録できない" do
@@ -20,29 +28,82 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Email can't be blank")
      end
 
+     it '重複したemailが存在する場合は登録できない' do
+        @user.email
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+      end
+
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+
       it "パスワードが空だと登録できない" do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
 
-      it "苗字(全角)が空だと登録できない" do
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '00000'
+        @user.password_confirmation = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it "passwordが6文字以上であれば登録できる" do
+        @user.password = '000000'
+        @user.password_confirmation = '000000'
+        @user.valid?
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end    
+
+      it "姓(全角)が空だと登録できない" do
         @user.family_name = ''
         @user.valid?
       end
 
-      it "名前(全角)が空だと登録できない" do
+      it '姓が全角でなければ登録できない' do
+        @user.family_name = "ｱｲｳｴｵ"
+        @user.valid?
+      end
+
+      it "名(全角)が空だと登録できない" do
         @user.first_name = ''
         @user.valid?
       end
 
-      it "苗字フリガナ(全角）が空だと登録できない" do
+      it '名が全角でなければ登録できない' do
+        @user.first_name = "ｱｲｳｴｵ"
+        @user.valid?
+      end
+
+      it "姓のフリガナ(全角）が空だと登録できない" do
         @user.family_name_furigana = ''
         @user.valid?
       end
 
-      it "名前フリガナ(全角)が空だと登録できない" do
+      it '姓のフリガナが全角カタカナでなければ登録できない' do
+        @user.family_name_furigana = "あいうえお"
+        @user.valid?
+      end
+
+      it "名のフリガナ(全角)が空だと登録できない" do
         @user.first_name_furigana = ''
+        @user.valid?
+      end
+
+      it '名のフリガナが全角カタカナでなければ登録できない' do
+        @user.first_name_furigana = "あいうえお"
         @user.valid?
       end
 
