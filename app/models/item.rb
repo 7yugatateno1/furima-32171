653ec27303f43577481bece04464d658174e0f50
7yugatateno1 <7yuga1@gmail.com>
2023-02-08 16:46:07 +0900

    Itemsテーブルに画像ファイルを紐付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   validates :name, :text, :category_id, :condition_id, :delivery_cost_id, :sender_area_id, :delivery_day_id, :price, :user, presence: true
   belongs_to :user
   has_one :buy_record
+  has_one_attached :image
 end
