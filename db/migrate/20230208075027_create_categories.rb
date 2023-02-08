76374e895c8098f35df09a309b62c6ce38fd005e
7yugatateno1 <7yuga1@gmail.com>
2023-02-08 17:06:45 +0900

    Categoryモデルへのデータ定義

--- db/migrate/20230208075027_create_categories.rb
+++ db/migrate/20230208075027_create_categories.rb
@@ -1,7 +1,9 @@
 class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
-
+      t.string     :name        , null: false
+      t.text       :text         , null: false
+      t.integer    :category_id     , null: false
       t.timestamps
     end
   end
