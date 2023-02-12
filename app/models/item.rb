dc30479c318356e996de18c0704d50aa5e341965
7yugatateno1 <7yuga1@gmail.com>
2023-02-12 21:08:57 +0900

    ビュー編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ApplicationRecord
-  include ActiveHash::Associations
   validates :name, :text, :category_id, :condition_id, :delivery_cost_id, :sender_area_id, :delivery_day_id, :price, :image, presence: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -8,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :delivery_day
   belongs_to :sender_area
   belongs_to :user
-  #has_one :buy_record
+  has_one :buy_record
   has_one_attached :image
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to:300, less_than_or_equal_to:9_999_999 }
   VALID_PRICE_REGEX = /\A[0-9]+\z/
