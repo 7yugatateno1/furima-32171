f320d1f8e864374e124d906e3483e73be75bd632
7yugatateno1 <7yuga1@gmail.com>
2023-02-13 20:19:04 +0900

    before_actionにまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :find_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at desc")
@@ -20,11 +21,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def edit
-    @item = Item.find(params[:id])
 
     if @item.user_id != current_user.id
       redirect_to root_path
@@ -34,8 +34,6 @@ class ItemsController < ApplicationController
 
   def update
     
-    @item = Item.find(params[:id])
-
     if @item.update(item_params)
       redirect_to action: :show
     else
@@ -54,4 +52,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end
